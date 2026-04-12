294815b064a9e9035351b45492d00da49843144b
Carlos Javier Vidal Guerrero <69587487+outcast2020@users.noreply.github.com>
2026-04-11 21:42:42 -0300

    Add Firebase web app configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,12 @@
-window.INANNA_FIREBASE_CONFIG = window.INANNA_FIREBASE_CONFIG || null;
+window.INANNA_FIREBASE_CONFIG = window.INANNA_FIREBASE_CONFIG || {
+  apiKey: "AIzaSyCfZs_tP8DqbZhbNNeX7rp8i9Plqw4gn9E",
+  authDomain: "inanna-ia.firebaseapp.com",
+  projectId: "inanna-ia",
+  storageBucket: "inanna-ia.firebasestorage.app",
+  messagingSenderId: "444542195295",
+  appId: "1:444542195295:web:cf8d174417d703866c2dea",
+  measurementId: "G-P6WM1FGQ24"
+};
 
 window.INANNA_FIREBASE_OPTIONS = Object.assign(
   {
